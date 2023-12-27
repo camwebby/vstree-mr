7ed2f396a6ede088000f3b9010cbeb6976f217b5
Cameron <hello@cameronwebby.com>
2023-12-27 16:11:29 +0000

    Add v analytics to landing

--- apps/landing/src/app/layout.tsx
+++ apps/landing/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -36,7 +37,10 @@ export default function RootLayout({
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
