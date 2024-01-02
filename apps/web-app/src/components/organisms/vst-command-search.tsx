dc5f6e4e5aff0cdc922dd1085ffeca2f80293c59
Cameron <hello@cameronwebby.com>
2024-01-02 14:02:04 +0000

    Fix search

--- apps/web-app/src/components/organisms/vst-command-search.tsx
+++ apps/web-app/src/components/organisms/vst-command-search.tsx
@@ -5,6 +5,8 @@ import {
   Badge,
   Dialog,
   DialogContent,
+  DialogHeader,
+  DialogTitle,
   Input,
   Separator,
   Table,
@@ -26,14 +28,15 @@ export const VstSearchCommandMenu = ({
   onVstClick: (vst: Vst) => void;
   onOpenChange: (open: boolean) => void;
 }) => {
-  const [enabledQuery, setEnableQuery] = useState(false);
-
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [debouncedSearch] = useDebouncedValue(searchTerm, 200);
 
-  const { data, isLoading } = api.vsts.getAll.useQuery(undefined, {
-    enabled: enabledQuery,
-  });
+  const { data, isLoading, isInitialLoading } = api.vsts.getAll.useQuery(
+    undefined,
+    {
+      enabled: debouncedSearch !== "",
+    },
+  );
 
   const filteredResults = useMemo(() => {
     if (debouncedSearch === "") {
@@ -45,46 +48,51 @@ export const VstSearchCommandMenu = ({
     });
   }, [data, debouncedSearch]);
 
+  const maxLength = 12;
+
   return (
     <Dialog
       open={open}
       onOpenChange={(v) => {
         onOpenChange(v);
-        setEnableQuery(true);
       }}
     >
       <DialogContent className="max-h-[55vh] overflow-y-scroll p-0">
-        {isLoading ? (
+        <DialogHeader className="sticky top-0 z-20 border-b bg-background/70 p-6 backdrop-blur-md">
+          <DialogTitle className="mb-2">Search</DialogTitle>
+          <Input
+            className=""
+            placeholder="Search for VSTs..."
+            value={searchTerm}
+            onInput={(event) => setSearchTerm(event.currentTarget.value)}
+          />
+        </DialogHeader>
+        {isInitialLoading ? (
           <>
-            <Loader2 className="m-4 animate-spin" />
+            <Loader2 className="mx-6 mb-6 animate-spin" />
           </>
         ) : (
           <>
-            <Input
-              className="sticky top-0 z-20 border-none p-5"
-              placeholder="Search for VSTs..."
-              value={searchTerm}
-              onInput={(event) => setSearchTerm(event.currentTarget.value)}
-            />
-
             <Table>
               <TableBody>
-                {filteredResults?.length === 0 && (
-                  <TableRow className="flex cursor-pointer items-center justify-between">
-                    <TableCell className="flex items-center gap-x-2">
-                      <span className="text-muted-foreground">
-                        No results found.
-                      </span>
-                    </TableCell>
-                  </TableRow>
-                )}
-                {filteredResults?.map((vst) => (
+                {filteredResults?.length === 0 &&
+                  searchTerm !== "" &&
+                  !isInitialLoading && (
+                    <TableRow className="flex cursor-pointer items-center justify-between">
+                      <TableCell className="flex items-center gap-x-2">
+                        <span className="text-muted-foreground">
+                          No results found.
+                        </span>
+                      </TableCell>
+                    </TableRow>
+                  )}
+                {filteredResults?.slice(0, maxLength).map((vst) => (
                   <TableRow
                     key={vst.id}
                     onClick={() => {
                       onVstClick(vst);
                     }}
-                    className="flex flex-nowrap cursor-pointer items-center justify-between"
+                    className="flex cursor-pointer flex-nowrap items-center justify-between"
                   >
                     <TableCell className="flex items-center gap-x-2">
                       <Avatar className="mr-2 h-6 w-6">
@@ -93,7 +101,7 @@ export const VstSearchCommandMenu = ({
                       </Avatar>
                       {vst.name}
                     </TableCell>
-                    <TableCell className="space-x-1 flex-nowrap whitespace-nowrap">
+                    <TableCell className="flex-nowrap space-x-1 whitespace-nowrap">
                       {vst.tags.slice(0, 2).map((t) => (
                         <Badge key={t} variant={"secondary"}>
                           {t}
@@ -107,6 +115,15 @@ export const VstSearchCommandMenu = ({
                     </TableCell>
                   </TableRow>
                 ))}
+                {filteredResults?.length > maxLength && (
+                  <TableRow className="flex items-center justify-between">
+                    <TableCell className="flex items-center gap-x-2">
+                      <span className="text-muted-foreground">
+                        {filteredResults?.length - maxLength} more...
+                      </span>
+                    </TableCell>
+                  </TableRow>
+                )}
               </TableBody>
             </Table>
           </>
